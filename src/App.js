498f80ce8460877bd648c5a1893ac76f671f9ec8
Aruna <arunamedapati2003@gmail.com>
2024-08-20 15:56:19 +0530

    one

--- src/App.js
+++ src/App.js
@@ -25,12 +25,12 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
                <Col sm={2} className='Col1'> <Navbar/> </Col>
                <Col sm={10} className='Col2'>
                <Routes>
-                <Route path='/react'exact Component={Home}/>
-                <Route path='/about'exact Component={About}/>
-                <Route path='/services'exact Component={Services}/>
-                <Route path='/contact'exact Component={Contact}/>
+                <Route path='/react' exact Component={Home}/>
+                <Route path='/about' exact Component={About}/>
+                <Route path='/services' exact Component={Services}/>
+                <Route path='/contact' exact Component={Contact}/>
                 <Route  path='/counter' exact Component={Counter}/>
-                <Route  path='/counter1' exact Component={counter1}/>
+                <Route  path='/counter1'  exact Component={counter1}/>
                </Routes>
                </Col>
 
